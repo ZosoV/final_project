7dbaec453a3d873cd48ab3a58f475e6a65b4ef15
ZosoV <hoskarg9619@gmail.com>
2024-08-01 14:47:31 +0100

    setting new experiments with learning rate 0.001

--- dqn_pixels/run_sweep.sh
+++ dqn_pixels/run_sweep.sh
@@ -20,37 +20,37 @@ for seed in "${seeds[@]}"; do
     # python dqn_cartpole.py -m \
     #     env.seed=$seed \
     #     optim.max_grad_norm=100
-    #     exp_name=DQN_pixels_max_grad_norm_100
+    #     exp_name=DQN_pixels_lr_0_001_max_grad_norm_100
 
     # python dqn_cartpole.py -m \
     #     env.seed=$seed \
     #     buffer.batch_size=128 \
-    #     exp_name=DQN_pixels_batch_size_128
+    #     exp_name=DQN_pixels_lr_0_001_batch_size_128
 
     # python dqn_cartpole.py -m \
     #     env.seed=$seed \
     #     buffer.batch_size=64 \
-    #     exp_name=DQN_pixels_batch_size_64
+    #     exp_name=DQN_pixels_lr_0_001_batch_size_64
 
     python dqn_cartpole.py -m \
         env.seed=$seed \
         loss.hard_update_freq=5000 \
-        exp_name=DQN_pixels_hard_update_freq_5000
+        exp_name=DQN_pixels_lr_0_001_hard_update_freq_5000
 
     python dqn_cartpole.py -m \
         env.seed=$seed \
         loss.hard_update_freq=500 \
-        exp_name=DQN_pixels_hard_update_freq_500
+        exp_name=DQN_pixels_lr_0_001_hard_update_freq_500
     
     python dqn_cartpole.py -m \
         env.seed=$seed \
         buffer.buffer_size=200000 \
-        exp_name=DQN_pixels_buffer_size_200_000
+        exp_name=DQN_pixels_lr_0_001_buffer_size_200_000
 
     python dqn_cartpole.py -m \
         env.seed=$seed \
         buffer.buffer_size=100000 \
-        exp_name=DQN_pixels_buffer_size_100_000
+        exp_name=DQN_pixels_lr_0_001_buffer_size_100_000
 
 
 done
