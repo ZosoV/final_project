d314cf2feafe8419adf5ef409d598ae32e44d81e
ZosoV <hoskarg9619@gmail.com>
2024-08-09 16:51:14 +0100

    Setting experiments

--- dqn_pixels/run_sweep.sh
+++ dqn_pixels/run_sweep.sh
@@ -26,35 +26,4 @@ for seed in "${seeds[@]}"; do
         buffer.prioritized_replay=True \
         optim.scheduler.active=True \
         exp_name=DQN_pixels_PER_alpha_0_6_beta_0_4_scheduler_True
-
-    # python dqn_cartpole.py -m \
-    #     env.seed=$seed \
-    #     buffer.batch_size=128 \
-    #     exp_name=DQN_pixels_lr_0_001_batch_size_128
-
-    # python dqn_cartpole.py -m \
-    #     env.seed=$seed \
-    #     buffer.batch_size=64 \
-    #     exp_name=DQN_pixels_lr_0_001_batch_size_64
-
-    # python dqn_cartpole.py -m \
-    #     env.seed=$seed \
-    #     loss.hard_update_freq=500 \
-    #     exp_name=DQN_pixels_lr_0_001_hard_update_freq_500
-
-    # python dqn_cartpole.py -m \
-    #     env.seed=$seed \
-    #     loss.hard_update_freq=5000 \
-    #     exp_name=DQN_pixels_lr_0_001_hard_update_freq_5000
-    
-    # python dqn_cartpole.py -m \
-    #     env.seed=$seed \
-    #     buffer.buffer_size=200000 \
-    #     exp_name=DQN_pixels_lr_0_001_buffer_size_200_000
-
-    # python dqn_cartpole.py -m \
-    #     env.seed=$seed \
-    #     buffer.buffer_size=100000 \
-    #     exp_name=DQN_pixels_lr_0_001_buffer_size_100_000
-
 done
--- dqn_pixels_mico/run_sweep.sh
+++ dqn_pixels_mico/run_sweep.sh
@@ -7,56 +7,10 @@ seeds=(118398) #676190 786456 171936 887739 919409 711872 442081 189061 117840)
 # Loop over each seed
 for seed in "${seeds[@]}"; do
 
-    # python dqn_cartpole.py -m \
-    #     env.seed=$seed \
-    #     exp_name=DQN_pixels_MICO_num_updates_1
-
-    # python dqn_cartpole.py -m \
-    #     env.seed=$seed \
-    #     buffer.prioritized_replay=True \
-    #     exp_name=DQN_pixels_MICO_aaBPER_alpha_0_6_beta_0_4_priority_1
-
-    # python dqn_cartpole.py -m \
-    #     env.seed=$seed \
-    #     buffer.prioritized_replay=True \
-    #     buffer.priority_alpha=0.5 \
-    #     buffer.alpha=0.6 \
-    #     buffer.beta=0.4 \
-    #     optim.lr=0.0005 \
-    #     exp_name=DQN_pixels_MICO_aaBPER_alpha_0_6_beta_0_4_pralpha_0_5_lr_0_0005
-
-    # python dqn_cartpole.py -m \
-    #     env.seed=$seed \
-    #     buffer.prioritized_replay=True \
-    #     buffer.priority_alpha=0.5 \
-    #     buffer.alpha=0.6 \
-    #     buffer.beta=0.4 \
-    #     optim.lr=0.0001 \
-    #     exp_name=DQN_pixels_MICO_aaBPER_alpha_0_6_beta_0_4_pralpha_0_5_lr_0_0001
-
     # NOTE: In theory, as the q_loss becomes more stable we could play with the learning rate
     # It seems that the learning rate need a high value in the beginning a low value in the end
     # so we could use a scheduler to decrease the learning rate
     # As the learning is more stable we can do that.
-
-    # TODO: Try this two in this computer and try the original for more time steps
-    # TODO: Try to normalize the distance
-    # python dqn_cartpole.py -m \
-    #     env.seed=$seed \
-    #     buffer.prioritized_replay=True \
-    #     buffer.mico_priority.priority_weight=0.5 \
-    #     buffer.alpha=0.6 \
-    #     buffer.beta=0.4 \
-    #     exp_name=DQN_pixels_MICO_aaBPER_alpha_0_6_beta_0_4_prweight_0_5
-
-    # python dqn_cartpole.py -m \
-    #     env.seed=$seed \
-    #     buffer.prioritized_replay=True \
-    #     buffer.mico_priority.priority_weight=0.5 \
-    #     buffer.mico_priority.normalize_priorities=True \
-    #     buffer.alpha=0.6 \
-    #     buffer.beta=0.4 \
-    #     exp_name=DQN_pixels_MICO_aaBPER_alpha_0_6_beta_0_4_prweight_0_5_norm_priorities_True
 
     python dqn_cartpole.py -m \
         env.seed=$seed \
@@ -64,22 +18,29 @@ for seed in "${seeds[@]}"; do
         buffer.mico_priority.priority_weight=0.5 \
         buffer.alpha=0.6 \
         buffer.beta=0.4 \
+        buffer.mico_priority.normalize_priorities=True \
         optim.scheduler.active=True \
-        exp_name=DQN_pixels_MICO_aaBPER_alpha_0_6_beta_0_4_prweight_0_5_scheduler_True
-
-
-    # In theory, I could increase the beta to account for the bias because of the same MICO loss
-    # will encourage exploration later on [Check this. I'm not sure if this is true]
-    python dqn_cartpole.py -m \
-        env.seed=$seed \
-        buffer.prioritized_replay=True \
-        buffer.mico_priority.priority_weight=0.5 \
-        buffer.alpha=0.6 \
-        buffer.beta=0.6 \
-        exp_name=DQN_pixels_MICO_aaBPER_alpha_0_6_beta_0_6_prweight_0_5
+        exp_name=DQN_pixels_MICO_aaBPER_alpha_0_6_beta_0_4_prweight_0_5_scheduler_True_norm_priorities_True
 
     # python dqn_cartpole.py -m \
     #     env.seed=$seed \
-    #     exp_name=DQN_pixels_MICO
+    #     buffer.prioritized_replay=True \
+    #     buffer.mico_priority.priority_weight=0.5 \
+    #     buffer.alpha=0.6 \
+    #     buffer.beta=0.4 \
+    #     optim.scheduler.active=True \
+    #     exp_name=DQN_pixels_MICO_aaBPER_alpha_0_6_beta_0_4_prweight_0_5_scheduler_True
+
+    # In theory, I could increase the beta to account for the bias because of the same MICO loss
+    # will encourage exploration later on [Check this. I'm not sure if this is true]
+    # python dqn_cartpole.py -m \
+    #     env.seed=$seed \
+    #     buffer.prioritized_replay=True \
+    #     buffer.mico_priority.priority_weight=0.5 \
+    #     buffer.alpha=0.6 \
+    #     buffer.beta=0.6 \
+    #     exp_name=DQN_pixels_MICO_aaBPER_alpha_0_6_beta_0_6_prweight_0_5
+
+    # TODO: Check two strategies: with priority weight [0.5, 0.75, 1.0] and normalize priorities [True, False]
     
 done
