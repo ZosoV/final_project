6bdde7aa2dd66785b24ee2694b4669f3de1c125f
ZosoV <hoskarg9619@gmail.com>
2024-08-14 11:43:00 +0100

    Setting last configurations

--- dqn_pixels/run_sweep.sh
+++ dqn_pixels/run_sweep.sh
@@ -7,9 +7,9 @@ seeds=(118398 676190 786456 171936 887739)
 # Loop over each seed
 for seed in "${seeds[@]}"; do
 
-    # python dqn_cartpole.py -m \
-    #     env.seed=$seed \
-    #     exp_name=DQN_pixels_baseline_seed_$seed \
+    python dqn_cartpole.py -m \
+        env.seed=$seed \
+        exp_name=DQN_pixels_baseline_seed_$seed \
     
     python dqn_cartpole.py -m \
         env.seed=$seed \
