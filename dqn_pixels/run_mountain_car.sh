11d05c8fd1ace5986395454ccef1212c6782c47b
ZosoV <hoskarg9619@gmail.com>
2024-08-20 11:49:12 +0100

    experiments

--- dqn_pixels/run_mountain_car.sh
+++ dqn_pixels/run_mountain_car.sh
@@ -6,6 +6,7 @@ seeds=(676190 786456) #171936 887739)
 python dqn.py -m \
     env.seed=118398 \
     env.env_name=MountainCar-v0 \
+    loss.double_dqn=True \
     logger.project_name=dqn_pixels_mountain_car_v0_sweep \
     buffer.prioritized_replay=True \
     exp_name=DQN_pixels_PER_seed_118398
@@ -15,12 +16,14 @@ for seed in "${seeds[@]}"; do
 
     python dqn.py -m \
         env.seed=$seed \
+        loss.double_dqn=True \
         env.env_name=MountainCar-v0 \
         logger.project_name=dqn_pixels_mountain_car_v0_sweep \
         exp_name=DQN_pixels_baseline_seed_$seed
     
     python dqn.py -m \
         env.seed=$seed \
+        loss.double_dqn=True \
         env.env_name=MountainCar-v0 \
         logger.project_name=dqn_pixels_mountain_car_v0_sweep \
         buffer.prioritized_replay=True \
