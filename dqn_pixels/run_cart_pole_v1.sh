dfdd6e9e5aaf1758464c4a3e69b601b346e8f779
ZosoV <hoskarg9619@gmail.com>
2024-08-20 20:32:03 +0100

    Config of cartpole v1

--- dqn_pixels/run_cart_pole_v1.sh
+++ dqn_pixels/run_cart_pole_v1.sh
@@ -7,14 +7,12 @@ for seed in "${seeds[@]}"; do
 
     python dqn.py -m \
         env.seed=$seed \
-        # loss.double_dqn=True \
         env.env_name=CartPole-v1 \
         logger.project_name=dqn_pixels_cart_pole_v1_sweep \
         exp_name=DQN_pixels_baseline_seed_$seed
     
     python dqn.py -m \
         env.seed=$seed \
-        # loss.double_dqn=True \
         env.env_name=CartPole-v1 \
         logger.project_name=dqn_pixels_cart_pole_v1_sweep \
         buffer.prioritized_replay=True \
