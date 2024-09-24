df6cc5fb857fbbf7e216f4d6860595bbb9aedc1e
ZosoV <hoskarg9619@gmail.com>
2024-09-24 18:17:06 +0100

    Experiments

--- dqn_pixels/run_mountain_car.sh
+++ dqn_pixels/run_mountain_car.sh
@@ -1,22 +1,21 @@
 #!/bin/bash
 
-seeds=(676190 786456) #171936 887739)
+seeds=(118398 676190 786456 171936 887739) # 919409 711872 442081 189061 117840)
+
 
 # Loop over each seed
 for seed in "${seeds[@]}"; do
 
     python dqn.py -m \
         env.seed=$seed \
-        loss.double_dqn=True \
         env.env_name=MountainCar-v0 \
-        logger.project_name=dqn_pixels_mountain_car_v0_sweep \
+        logger.project_name=dqn_pixels_mountain_car_v0_sweep_2 \
         exp_name=DQN_pixels_baseline_seed_$seed
     
     python dqn.py -m \
         env.seed=$seed \
-        loss.double_dqn=True \
         env.env_name=MountainCar-v0 \
-        logger.project_name=dqn_pixels_mountain_car_v0_sweep \
+        logger.project_name=dqn_pixels_mountain_car_v0_sweep_2 \
         buffer.prioritized_replay=True \
         exp_name=DQN_pixels_PER_seed_$seed
 
--- dqn_pixels/run_sweep.sh
+++ dqn_pixels/run_sweep.sh
@@ -1,6 +1,22 @@
 #!/bin/bash
 
+seeds=(118398 676190 786456 171936 887739) # 919409 711872 442081 189061 117840)
+# seeds=(171936 887739) # 919409 711872 442081 189061 117840)
 
-run_mountain_car.sh
+# Loop over each seed
+for seed in "${seeds[@]}"; do
 
-run_car_racing.sh
+    python dqn.py -m \
+        env.seed=$seed \
+        env.env_name=LunarLander-v2 \
+        logger.project_name=dqn_pixels_lunar_lander_v2_sweep_2 \
+        exp_name=DQN_pixels_baseline_seed_$seed
+    
+    python dqn.py -m \
+        env.seed=$seed \
+        env.env_name=LunarLander-v2 \
+        logger.project_name=dqn_pixels_lunar_lander_v2_sweep_2 \
+        buffer.prioritized_replay=True \
+        exp_name=DQN_pixels_PER_seed_$seed
+
+done
