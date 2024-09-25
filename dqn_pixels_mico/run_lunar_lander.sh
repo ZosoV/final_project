40cd064c8c5403743ac64bbcb6994b372df4f925
ZosoV <hoskarg9619@gmail.com>
2024-09-25 13:20:22 +0100

    Preparing experiments

--- dqn_pixels_mico/run_lunar_lander.sh
+++ dqn_pixels_mico/run_lunar_lander.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
+seeds=(786456 171936 887739) # 919409 711872 442081 189061 117840)
+
+priority_weights=(0.0 1.0) # Note: 1.0 I just need to calculate 2 seed more
+# priority_weights=(1.0) # Note: 1.0 I just need to calculate 2 seed more
+
+# Loop over each seed
+for seed in "${seeds[@]}"; do
+
+    python dqn_mico.py -m \
+        env.seed=$seed \
+        env.env_name="LunarLander-v2" \
+        logger.project_name="dqn_pixels_lunar_lander_v2_sweep_2" \
+        exp_name=DQN_MICO_seed_$seed
+    
+done
+
 seeds=(118398 676190 786456 171936 887739) # 919409 711872 442081 189061 117840)
 
 priority_weights=(0.0 1.0) # Note: 1.0 I just need to calculate 2 seed more
@@ -18,9 +34,7 @@ for seed in "${seeds[@]}"; do
             buffer.mico_priority.priority_type="all_vs_all" \
             buffer.mico_priority.priority_weight=$priority_weight \
             buffer.mico_priority.normalize_priorities=False \
-            logger.cumulative_reward="sum" \
-            loss.double_dqn=True \
-            exp_name=DQN_pixels_MICO_BPER_aa_priority_weight_seed_$seed
+            exp_name=DQN_MICO_BPER_aa_seed_$seed
 
     done
     
@@ -41,15 +55,13 @@ for seed in "${seeds[@]}"; do
         
         python dqn_mico.py -m \
             env.seed=$seed \
-            env.env_name="MountainCar-v0" \
-            logger.project_name="dqn_pixels_mountain_car_v0_sweep" \
+            env.env_name="LunarLander-v2" \
+            logger.project_name="dqn_pixels_lunar_lander_v2_sweep_2" \
             buffer.prioritized_replay=True \
             buffer.mico_priority.priority_type="current_vs_next" \
             buffer.mico_priority.priority_weight=$priority_weight \
             buffer.mico_priority.normalize_priorities=False \
-            logger.cumulative_reward="sum" \
-            loss.double_dqn=True \
-            exp_name=DQN_pixels_MICO_BPER_aa_priority_weight_seed_$seed
+            exp_name=DQN_MICO_BPER_cn_seed_$seed
 
     done
     
