031ef2797e83e5536bf1e0a5134ef58d1d90e1bd
ZosoV <hoskarg9619@gmail.com>
2024-10-07 11:41:19 +0100

    Checking DoubleDQN result

--- dqn_pixels/run_sweep.sh
+++ dqn_pixels/run_sweep.sh
@@ -9,14 +9,14 @@ for seed in "${seeds[@]}"; do
     python dqn.py -m \
         env.seed=$seed \
         env.env_name=LunarLander-v2 \
-        logger.project_name=dqn_pixels_lunar_lander_v2_sweep_2 \
+        logger.project_name=dqn_pixels_lunar_lander_v2_sweep_3 \
         exp_name=DQN_pixels_baseline_seed_$seed
     
-    python dqn.py -m \
-        env.seed=$seed \
-        env.env_name=LunarLander-v2 \
-        logger.project_name=dqn_pixels_lunar_lander_v2_sweep_2 \
-        buffer.prioritized_replay=True \
-        exp_name=DQN_pixels_PER_seed_$seed
+    # python dqn.py -m \
+    #     env.seed=$seed \
+    #     env.env_name=LunarLander-v2 \
+    #     logger.project_name=dqn_pixels_lunar_lander_v2_sweep_2 \
+    #     buffer.prioritized_replay=True \
+    #     exp_name=DQN_pixels_PER_seed_$seed
 
 done
--- dqn_pixels_mico/run_lunar_lander.sh
+++ dqn_pixels_mico/run_lunar_lander.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-seeds=(118398 676190 786456 171936 887739) # 919409 711872 442081 189061 117840)
+# seeds=(118398 676190 786456 171936 887739) # 919409 711872 442081 189061 117840)
+seeds=(118398 676190) # 786456 171936 887739) # 919409 711872 442081 189061 117840)
 
-priority_weights=(0.0 1.0) # Note: 1.0 I just need to calculate 2 seed more
+# priority_weights=(0.0 1.0) # Note: 1.0 I just need to calculate 2 seed more
 # priority_weights=(1.0) # Note: 1.0 I just need to calculate 2 seed more
 
 # Loop over each seed
@@ -16,29 +17,30 @@ for seed in "${seeds[@]}"; do
     
 done
 
-# seeds=(676190 786456 171936 887739) # 919409 711872 442081 189061 117840)
+# seeds=(118398 676190 786456 171936 887739) # 919409 711872 442081 189061 117840)
+seeds=(118398 676190) # 786456 171936 887739) # 919409 711872 442081 189061 117840)
 
 # priority_weights=(0.0 1.0) # Note: 1.0 I just need to calculate 2 seed more
-# # priority_weights=(1.0) # Note: 1.0 I just need to calculate 2 seed more
+priority_weights=(1.0) # Note: 1.0 I just need to calculate 2 seed more
 
-# # Loop over each seed
-# for seed in "${seeds[@]}"; do
+# Loop over each seed
+for seed in "${seeds[@]}"; do
 
-#     for priority_weight in "${priority_weights[@]}"; do
+    for priority_weight in "${priority_weights[@]}"; do
         
-#         python dqn_mico.py -m \
-#             env.seed=$seed \
-#             env.env_name="LunarLander-v2" \
-#             logger.project_name="dqn_pixels_lunar_lander_v2_sweep_2" \
-#             buffer.prioritized_replay=True \
-#             buffer.mico_priority.priority_type="all_vs_all" \
-#             buffer.mico_priority.priority_weight=$priority_weight \
-#             buffer.mico_priority.normalize_priorities=False \
-#             exp_name=DQN_MICO_BPER_aa_seed_$seed
+        python dqn_mico.py -m \
+            env.seed=$seed \
+            env.env_name="LunarLander-v2" \
+            logger.project_name="dqn_pixels_lunar_lander_v2_sweep_3" \
+            buffer.prioritized_replay=True \
+            buffer.mico_priority.priority_type="all_vs_all" \
+            buffer.mico_priority.priority_weight=$priority_weight \
+            buffer.mico_priority.normalize_priorities=False \
+            exp_name=DDQN_MICO_BPER_aa_seed_$seed
 
-#     done
+    done
     
-# done
+done
 
 # seeds=(118398 676190 786456 171936 887739) # 919409 711872 442081 189061 117840)
 
@@ -49,21 +51,21 @@ done
 # # priority_weights=(0.1) #1.0) # Note: 1.0 I just need to calculate 2 seed more
 # # # priority_weights=(1.0) # Note: 1.0 I just need to calculate 2 seed more
 
-# # Loop over each seed
-# for seed in "${seeds[@]}"; do
+# Loop over each seed
+for seed in "${seeds[@]}"; do
 
-#     for priority_weight in "${priority_weights[@]}"; do
+    for priority_weight in "${priority_weights[@]}"; do
         
-#         python dqn_mico.py -m \
-#             env.seed=$seed \
-#             env.env_name="LunarLander-v2" \
-#             logger.project_name="dqn_pixels_lunar_lander_v2_sweep_2" \
-#             buffer.prioritized_replay=True \
-#             buffer.mico_priority.priority_type="current_vs_next" \
-#             buffer.mico_priority.priority_weight=$priority_weight \
-#             buffer.mico_priority.normalize_priorities=False \
-#             exp_name=DQN_MICO_BPER_cn_seed_$seed
+        python dqn_mico.py -m \
+            env.seed=$seed \
+            env.env_name="LunarLander-v2" \
+            logger.project_name="dqn_pixels_lunar_lander_v2_sweep_2" \
+            buffer.prioritized_replay=True \
+            buffer.mico_priority.priority_type="current_vs_next" \
+            buffer.mico_priority.priority_weight=$priority_weight \
+            buffer.mico_priority.normalize_priorities=False \
+            exp_name=DQN_MICO_BPER_cn_seed_$seed
 
-#     done
+    done
     
-# done
+done
