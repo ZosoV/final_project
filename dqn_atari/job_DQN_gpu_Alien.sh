a158682a383301391887855f6517270aeb869148
ZosoV <hoskarg9619@gmail.com>
2025-03-24 01:47:31 +0000

    Adding gpu experiments

--- dqn_atari/job_DQN_gpu_Alien.sh
+++ dqn_atari/job_DQN_gpu_Alien.sh
@@ -5,11 +5,11 @@
 #SBATCH --time=5-00:00:00
 #SBATCH --mail-type=ALL
 #SBATCH --qos=bbgpu
-#SBATCH --cpus-per-task=28
+#SBATCH --cpus-per-task=36
 #SBATCH --nodes=1
-#SBATCH --mem-per-cpu=8GB
+#SBATCH --mem-per-cpu=6GB
 #SBATCH --account=giacobbm-bisimulation-rl
-#SBATCH --gres=gpu:a30:1
+#SBATCH --gres=gpu:a100:1
 #SBATCH --output="outputs/slurm-files/slurm-DQN-%A_%a.out"
 
 module purge; module load bluebear
@@ -23,7 +23,7 @@ module load tqdm/4.66.1-GCCcore-12.3.0
 
 GAME_NAME=Alien
 VARIANT=${VARIANT:-DQN}  # Default to DQN if no variant is specified
-CUSTOM_THREADS=14
+CUSTOM_THREADS=18
 ITERATIONS=40
 
 # Temporary scratch space for I/O efficiency
