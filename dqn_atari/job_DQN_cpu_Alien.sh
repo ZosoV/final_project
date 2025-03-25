0ac9dd9f4b08b9cbbe776168d6af941c44246dd1
ZosoV <hoskarg9619@gmail.com>
2025-03-25 23:36:02 +0000

    Trying a lot of cores

--- dqn_atari/job_DQN_cpu_Alien.sh
+++ dqn_atari/job_DQN_cpu_Alien.sh
@@ -5,15 +5,13 @@
 #SBATCH --time=10-00:00:00
 #SBATCH --qos=bbdefault
 #SBATCH --mail-type=ALL
-#SBATCH --nodes=1
-#SBATCH --cpus-per-task=32
-#SBATCH --mem-per-cpu=8GB
+#SBATCH --cpus-per-task=56
 #SBATCH --output="outputs/slurm-files/slurm-DQN-cpu-%A_%a.out"
 #SBATCH --constraint=sapphire
 
 GAME_NAME=Alien
 VARIANT=${VARIANT:-DQN}  # Default to DQN if no variant is specified
-CUSTOM_THREADS=24
+CUSTOM_THREADS=42
 ITERATIONS=201
 
 # Temporary scratch space for I/O efficiency
