aa40ca18e217c990fdf44113b70cc0141cb90759
ZosoV <hoskarg9619@gmail.com>
2025-03-30 20:43:02 +0100

    setting other gpu

--- dopamine_sandbox/job_Alien_metric_dqn_bper.sh
+++ dopamine_sandbox/job_Alien_metric_dqn_bper.sh
@@ -7,7 +7,7 @@
 #SBATCH --qos=bbgpu
 #SBATCH --cpus-per-task=14
 #SBATCH --account=giacobbm-bisimulation-rl
-#SBATCH --gres=gpu:a30:1
+#SBATCH --gres=gpu:a100:1
 #SBATCH --output="outputs/slurm-files/slurm-DQN-%A_%a.out"
 
 module purge; module load bluebear
